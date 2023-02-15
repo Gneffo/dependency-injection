766961a1b788338d7f44c0c9e4b7e54dfb80fbda
Michele Junior Cioffo <michelecioffo_98@hotmail.com>
2023-02-15 17:50:27 +0100

    Initial commit

---
+++ src/main/java/co/develhope/dependencyinjection/controllers/MyController.java
@@ -0,0 +1,28 @@
+package co.develhope.dependencyinjection.controllers;
+
+import co.develhope.dependencyinjection.services.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @Autowired
+    private MyService service;
+    @Autowired
+    public MyController(MyService service) {
+        System.out.println("MyController constructor has been called");
+        this.service = service;
+    }
+
+    @GetMapping
+    public String helloMsg(){
+        return "Hello, thanks for visiting us!";
+    }
+
+    @GetMapping("/getName")
+    public String getName(){
+        return service.getName();
+    }
+
+}
